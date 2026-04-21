c685d6bca8ee88113e2d291b55acc3f26c07e5dc
Chris Pamula <56594561+chris9515@users.noreply.github.com>
2026-04-21 23:04:24 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counts = {}
+        for i in s:
+            counts[i] = counts.get(i, 0) + 1
+        for i in t:
+            counts[i] = counts.get(i, 0) - 1
+        for i in counts.values():
+            if i!=0:
+                return False
+        return True
+        
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = list()
+        checks = dict()
+        for i in range(len(strs)):
+            passed = checks.get(i, False)
+            if passed:
+                continue
+            else:
+                checks[i] = True
+                temp = [strs[i]]
+            for j in range(i+1, len(strs)):
+                if self.isAnagram(strs[i], strs[j]):
+                    temp.append(strs[j])
+                    checks[j] = True
+            group.append(temp)
+        return group
+
+
+
+        
